d30e3d43a75d70cffc8cab18ad58b1e9b7fb54f7
Gabe <gabemendoza27@gmail.com>
2023-06-23 16:47:32 -0400

    6/22/2023

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 import sys
+from PyQt6 import QtCore
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton,QVBoxLayout,QHBoxLayout, QGridLayout, QLabel, QWidget, QLineEdit, QFrame
 from root import Error, Connect
 
 class Login(QMainWindow):
+
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MDC")
 
         #layout
-        global layout2
-        layout2 = QGridLayout( )
+        self.layout = QGridLayout( )
         labell = QLabel("MYSQL Database Creator")
-        layout2.addWidget(labell,0,0)
+        self.layout.addWidget(labell,0,0)
 
         #Username text and box
         self.labelu = QLabel("Username:")
-        layout2.addWidget(self.labelu,1,0)
+        self.layout.addWidget(self.labelu,1,0)
         self.usern = QLineEdit()
         self.usern.setFixedWidth(100)
-        layout2.addWidget(self.usern,1,1)
+        self.layout.addWidget(self.usern,1,1)
 
         #Password text and box
         labelp = QLabel("Password:")
-        layout2.addWidget(labelp,2,0)
+        self.layout.addWidget(labelp,2,0)
         self.passw = QLineEdit()
-        layout2.addWidget(self.passw,2,1)
+        self.passw.setEchoMode(QLineEdit.EchoMode.Password)
+        self.layout.addWidget(self.passw,2,1)
         self.passw.setFixedWidth(100)
 
         #Submit Button
         self.buttons = QPushButton("Submit")
         self.buttons.clicked.connect(self.submit)
-        layout2.addWidget(self.buttons,3,1)
+        self.layout.addWidget(self.buttons,3,1)
 
         widget = QWidget()
-        widget.setLayout(layout2)
+        widget.setLayout(self.layout)
         self.setMenuWidget(widget)
 
         #Window params
@@ -54,7 +57,7 @@ class Login(QMainWindow):
         except Error as e:
             labele = QLabel(str(e))
             labele.setStyleSheet("color: red;")
-            layout2.addWidget(labele,3,0)
+            self.layout.addWidget(labele,3,0)
             self.setFixedHeight(150)
 
 
@@ -63,6 +66,8 @@ class mainPage(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
+
         self.resize(600, 600)
         self.setFixedHeight(900)
         self.setFixedWidth(900)
@@ -72,17 +77,24 @@ class mainPage(QMainWindow):
         labelp = QLabel("Hello "+ (usernstring.text()).upper())
         self.Body.addWidget(labelp,0,0)
 
-        created = QPushButton("Create Database")
-        self.Body.addWidget(created,1,0)
-        created = QPushButton("Delete Database")
-        self.Body.addWidget(created,1,1)
-        created = QPushButton("Create Table")
-        self.Body.addWidget(created,1,2)
-        created = QPushButton("Drop Table")
-        self.Body.addWidget(created,1,3)
+        self.createdd = QPushButton("Create Database")
+        self.Body.addWidget(self.createdd,1,0)
+        self.createdd.clicked.connect(self.cDatabase)
 
-        created = QPushButton("Search")
-        self.Body.addWidget(created,2,0)
+        self.deleted = QPushButton("Delete Database")
+        self.Body.addWidget(self.deleted,1,1)
+        self.deleted.clicked.connect(self.dDatabase)
+
+        self.createdt = QPushButton("Create Table")
+        self.Body.addWidget(self.createdt,1,2)
+        self.createdt.clicked.connect(self.cTable)
+
+        self.deletet = QPushButton("Drop Table")
+        self.Body.addWidget(self.deletet,1,3)
+        self.deletet.clicked.connect(self.dTable)
+
+        self.search = QPushButton("Search")
+        self.Body.addWidget(self.search,2,0)
 
 
 
@@ -92,6 +104,136 @@ class mainPage(QMainWindow):
         widget1.setLayout(self.Body)
         self.setMenuWidget(widget1)
 
+
+    def cDatabase(self, checked):
+        self.w = createDatabase()
+        self.w.show()
+
+    def dDatabase(self, checked):
+        self.w = deleteDatabase()
+        self.w.show()
+
+    def cTable(self, checked):
+        self.w = createTable()
+        self.w.show()
+    
+    def dTable(self, checked):
+        self.w = dropTable()
+        self.w.show()
+    
+    def search(self, checked):
+        self.w = Search()
+        self.w.show()
+
+class createDatabase(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MDC")
+        layout = QGridLayout()
+        self.enterl = QLabel("Enter Database Name")
+        
+
+        self.entert = QLineEdit()
+        self.entert.setFixedWidth(150)
+
+        self.enterb = QPushButton("Enter")
+
+        layout.addWidget(self.enterl)
+        layout.addWidget(self.entert)
+        layout.addWidget(self.enterb)
+        self.setLayout(layout)
+
+        self.enterl.setFixedWidth(150)
+        self.setFixedHeight(105)
+        self.setFixedWidth(200)
+
+class deleteDatabase(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MDC")
+        layout = QGridLayout()
+        self.enterl = QLabel("Enter Database Name")
+        
+
+        self.entert = QLineEdit()
+        self.entert.setFixedWidth(150)
+
+        self.enterb = QPushButton("Enter")
+
+        layout.addWidget(self.enterl)
+        layout.addWidget(self.entert)
+        layout.addWidget(self.enterb)
+        self.setLayout(layout)
+
+        self.enterl.setFixedWidth(150)
+        self.setFixedHeight(105)
+        self.setFixedWidth(200)
+
+class createTable(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MDC")
+        layout = QGridLayout()
+        self.enterl = QLabel(" Enter Table Name")
+        
+
+        self.entert = QLineEdit()
+        self.entert.setFixedWidth(130)
+
+        self.enterb = QPushButton("Enter")
+
+        layout.addWidget(self.enterl)
+        layout.addWidget(self.entert)
+        layout.addWidget(self.enterb)
+        self.setLayout(layout)
+
+        self.enterl.setFixedWidth(130)
+        self.setFixedHeight(105)
+        self.setFixedWidth(200)
+
+class dropTable(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MDC")
+        layout = QGridLayout()
+        self.enterl = QLabel(" Enter Table Name")
+        
+
+        self.entert = QLineEdit()
+        self.entert.setFixedWidth(130)
+
+        self.enterb = QPushButton("Enter")
+
+        layout.addWidget(self.enterl)
+        layout.addWidget(self.entert)
+        layout.addWidget(self.enterb)
+        self.setLayout(layout)
+
+        self.enterl.setFixedWidth(130)
+        self.setFixedHeight(105)
+        self.setFixedWidth(200)
+
+class Search(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("MDC")
+        layout = QGridLayout()
+        self.enterl = QLabel(" Enter name")
+        
+
+        self.entert = QLineEdit()
+        self.entert.setFixedWidth(130)
+
+        self.enterb = QPushButton("Enter")
+
+        layout.addWidget(self.enterl)
+        layout.addWidget(self.entert)
+        layout.addWidget(self.enterb)
+        self.setLayout(layout)
+
+        self.enterl.setFixedWidth(130)
+        self.setFixedHeight(105)
+        self.setFixedWidth(200)
 
 
 app = QApplication(sys.argv)
